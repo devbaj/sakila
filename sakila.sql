9bb884fef750656ca68661d1ec1a1f55d413dfed
Brett Jory <bjory7@gmail.com>
2019-03-26 18:52:56 -0700

    checked answer to all, no errors. Cleaned up code for push.

--- sakila.sql
+++ sakila.sql
@@ -12,7 +12,7 @@ INNER JOIN film_category on film.film_id = film_category.film_id
 INNER JOIN category on film_category.category_id = category.category_id AND category.name = 'Comedy';
 
 -- 3
-select actor.actor_id, concat(' ', actor.first_name, actor.last_name) as actor_name, film.title, film.description, film.release_year
+select actor.actor_id, concat(actor.first_name, ' ', actor.last_name) as actor_name, film.title, film.description, film.release_year
 FROM film
 inner join film_actor on film.film_id = film_actor.film_id
 inner join actor on film_actor.actor_id = actor.actor_id and actor.actor_id = 5;
