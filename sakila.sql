5c941c810f01cc9fa5fcd4af52b5bf9bd7897521
Brett Jory <bjory7@gmail.com>
2019-03-26 18:03:13 -0700

    queried all films with actor_id = 5, returning film info

--- sakila.sql
+++ sakila.sql
@@ -8,3 +8,10 @@ select film.title, film.description, film.release_year, film.rating, film.specia
 FROM film
 INNER JOIN film_category on film.film_id = film_category.film_id
 INNER JOIN category on film_category.category_id = category.category_id AND category.name = 'Comedy';
+
+-- 3
+select actor.actor_id, concat(' ', actor.first_name, actor.last_name) as actor_name, film.title, film.description, film.release_year
+FROM film
+inner join film_actor on film.film_id = film_actor.film_id
+inner join actor on film_actor.actor_id = actor.actor_id and actor.actor_id = 5;
+
