5893bd37346c3af23b2396ba0a43e494e0b7e1ca
Brett Jory <bjory7@gmail.com>
2019-03-26 18:53:53 -0700

    minor fixes: capitalization, indents

--- sakila.sql
+++ sakila.sql
@@ -1,49 +1,132 @@
 use sakila;
 
 -- 1
-SELECT customer.first_name, customer.last_name, customer.email, address.address
-FROM customer
-JOIN address ON customer.address_id = address.address_id AND address.city_id = 312;
+SELECT 
+    customer.first_name,
+    customer.last_name,
+    customer.email,
+    address.address
+FROM
+    customer
+        JOIN
+    address ON customer.address_id = address.address_id
+        AND address.city_id = 312;
 
 -- 2
-select film.title, film.description, film.release_year, film.rating, film.special_features, category.name
-FROM film
-INNER JOIN film_category on film.film_id = film_category.film_id
-INNER JOIN category on film_category.category_id = category.category_id AND category.name = 'Comedy';
+SELECT 
+    film.title,
+    film.description,
+    film.release_year,
+    film.rating,
+    film.special_features,
+    category.name
+FROM
+    film
+        INNER JOIN
+    film_category ON film.film_id = film_category.film_id
+        INNER JOIN
+    category ON film_category.category_id = category.category_id
+        AND category.name = 'Comedy';
 
 -- 3
-select actor.actor_id, concat(actor.first_name, ' ', actor.last_name) as actor_name, film.title, film.description, film.release_year
-FROM film
-inner join film_actor on film.film_id = film_actor.film_id
-inner join actor on film_actor.actor_id = actor.actor_id and actor.actor_id = 5;
+SELECT 
+    actor.actor_id,
+    CONCAT(actor.first_name, ' ', actor.last_name) AS actor_name,
+    film.title,
+    film.description,
+    film.release_year
+FROM
+    film
+        INNER JOIN
+    film_actor ON film.film_id = film_actor.film_id
+        INNER JOIN
+    actor ON film_actor.actor_id = actor.actor_id
+        AND actor.actor_id = 5;
 
 -- 4
-select customer.first_name, customer.last_name, customer.email, address.address
-FROM customer
-inner join address on customer.address_id = address.address_id and (address.city_id = 1 OR address.city_id = 42 OR address.city_id = 312 OR address.city_id = 459)
-inner join store on customer.store_id = store.store_id and store.store_id = 1;
+SELECT 
+    customer.first_name,
+    customer.last_name,
+    customer.email,
+    address.address
+FROM
+    customer
+        INNER JOIN
+    address ON customer.address_id = address.address_id
+        AND (address.city_id = 1
+        OR address.city_id = 42
+        OR address.city_id = 312
+        OR address.city_id = 459)
+        INNER JOIN
+    store ON customer.store_id = store.store_id
+        AND store.store_id = 1;
 
 -- 5
-select film.title, film.description, film.release_year, film.rating, film.special_features
-FROM film
-join film_actor on film.film_id = film_actor.film_id and film_actor.actor_id = 15 and film.special_features like "%behind the scenes%" and film.rating = "G";
+SELECT 
+    film.title,
+    film.description,
+    film.release_year,
+    film.rating,
+    film.special_features
+FROM
+    film
+        JOIN
+    film_actor ON film.film_id = film_actor.film_id
+        AND film_actor.actor_id = 15
+        AND film.special_features LIKE '%behind the scenes%'
+        AND film.rating = 'G';
 
 -- 6
-select film.film_id, film.title, film_actor.actor_id, concat(actor.first_name, ' ', actor.last_name) as actor_name
-from film
-inner join film_actor on film.film_id = film_actor.film_id and film.film_id = 369
-inner join actor on film_actor.actor_id = actor.actor_id;
+SELECT 
+    film.film_id,
+    film.title,
+    film_actor.actor_id,
+    CONCAT(actor.first_name, ' ', actor.last_name) AS actor_name
+FROM
+    film
+        INNER JOIN
+    film_actor ON film.film_id = film_actor.film_id
+        AND film.film_id = 369
+        INNER JOIN
+    actor ON film_actor.actor_id = actor.actor_id;
 
 -- 7
-select film.title, film.description, film.release_year, film.rating, film.special_features, category.name
-from film
-inner join film_category on film.film_id =  film_category.film_id and film.rental_rate = 2.99
-inner join category on film_category.category_id = category.category_id and category.name = "Drama";
+SELECT 
+    film.title,
+    film.description,
+    film.release_year,
+    film.rating,
+    film.special_features,
+    category.name
+FROM
+    film
+        INNER JOIN
+    film_category ON film.film_id = film_category.film_id
+        AND film.rental_rate = 2.99
+        INNER JOIN
+    category ON film_category.category_id = category.category_id
+        AND category.name = 'Drama';
 
 -- 8
-select film.title, film.description, film.release_year, film.rating, film.special_features, category.name as genre, actor.first_name, actor.last_name
-from film
-inner join film_category on film.film_id = film_category.film_id
-inner join category on film_category.category_id = category.category_id and category.name = "Action"
-inner join film_actor on film.film_id = film_actor.film_id
-inner join actor on film_actor.actor_id = actor.actor_id and actor.first_name = "SANDRA" and actor.last_name = "KILMER";
+SELECT 
+    film.title,
+    film.description,
+    film.release_year,
+    film.rating,
+    film.special_features,
+    category.name AS genre,
+    actor.first_name,
+    actor.last_name
+FROM
+    film
+        INNER JOIN
+    film_category ON film.film_id = film_category.film_id
+        INNER JOIN
+    category ON film_category.category_id = category.category_id
+        AND category.name = 'Action'
+        INNER JOIN
+    film_actor ON film.film_id = film_actor.film_id
+        INNER JOIN
+    actor ON film_actor.actor_id = actor.actor_id
+        AND actor.first_name = 'SANDRA'
+        AND actor.last_name = 'KILMER';
